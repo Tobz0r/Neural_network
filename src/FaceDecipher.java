34ae6cb04df0dfdb11e5419213b289ebd9d80a97
Tobias Estefors <dv13tes@cs.umu.se>
2016-10-13 14:24:03 +0200

    eliashej

--- src/FaceDecipher.java
+++ src/FaceDecipher.java
@@ -5,6 +5,6 @@ public class FaceDecipher {
 
     public void deciphe(){
 
-        
+
     }
 }
--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -6,7 +6,9 @@ public class NeuralNetwork {
 
 
     public void run(){
-
+        while(true){
+            System.out.println("eliashej");
+        }
     }
 
 
